01663a145f2babcac51c793d4688794d9cbc67ac
Oscar ordoñez <oscargonz150@gmail.com>
2025-04-28 19:59:34 -0600

    ✨ | #08 | linkedin
    Se agrego una nueva tecnología y se modifico el apartado para enviar a LinkedIn personal

--- public/js/main.js
+++ public/js/main.js
@@ -7,31 +7,8 @@ const moonIcon = document.querySelector('.bx-moon');
 const userTheme = localStorage.getItem("Theme");
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-
-
 // Year footer
 let fecha = new Date();
 let year = fecha.getFullYear();
 let span = document.getElementById('year');
 span.innerHTML= year;
-
-
-//modal of copy my email address :p 
-function copiarTexto() {
-    var texto = document.getElementById("textoParaCopiar");
-
-    // Crear un área de texto oculta para copiar el texto
-    var textarea = document.createElement("textarea");
-    textarea.value = texto.value;
-    document.body.appendChild(textarea);
-
-    // Seleccionar y copiar el texto
-    textarea.select();
-    document.execCommand("copy");
-
-    // Eliminar el área de texto
-    document.body.removeChild(textarea);
-
-    // Mostrar un mensaje de éxito
-    alert("Texto copiado al portapapeles: " + texto.value);
-}
